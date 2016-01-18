3efc1a190a843ad5fc0bc3ebb7c97c22cc4d20e5
Luka Bozhich <luka.bozhich@gmail.com>
2016-01-18 14:16:23 +0200

    modified:   modules/api/controllers/Auth.php

--- modules/api/controllers/Auth.php
+++ modules/api/controllers/Auth.php
@@ -1,6 +1,22 @@
 <?php
 
-class Api_ConnectionsController extends Api_AdminControllerHelper {
-	public function addAction() {
+class Api_AuthController extends Api_AdminControllerHelper {
+	public function indexAction() {
+		$user = $this->getRequest()->user;
+		$pass = $this->getRequest()->pass;
+
+		$player_rs = Default_PlayersModel::get(array(
+			'user'     => $user,
+			'password' => Core_Security::generate($pass),
+		));
+
+		if (!$player_rs) {
+			return $this->addError('Invalid username and/or password');
+		}
+
+		$token = Default_UsersHelper::login($player_rs->id);
+		$this->addResponse(array(
+			'token' => $token
+		));
 	}
 }
